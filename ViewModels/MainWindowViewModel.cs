99fd1cafee55c5302f88577e23864fc79f6fee5c
PiepMatz <rappersinfan@gmail.com>
2025-04-14 12:44:16 +0200

    tech: Main: rewrite router message receiver

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,18 +4,39 @@
 public partial class MainWindowViewModel : ViewModelBase
 {
 	private readonly IMessenger _messenger = WeakReferenceMessenger.Default;
+	private readonly IServiceProvider _serviceProvider;
+	private readonly LogController _logController;
 
 	[ObservableProperty]
 	private ViewModelBase? _currentPage;
 
-	public MainWindowViewModel(IServiceProvider sP)
+	public MainWindowViewModel(IServiceProvider sP, LogController lc)
 	{
+		_serviceProvider = sP;
+		_logController = lc;
+		_messenger.Register<MainWindowViewModel, RouteMessage>(this, RouteToPage);
 		_currentPage = sP.GetRequiredService<SplashScreenViewModel>();
-		_messenger.Register<MainWindowViewModel, RouteMessage>(this, (_, route) =>
+	}
+
+	/// <summary>
+	/// Receiver for the RouteMessage, tries to route to the given page / service.
+	/// </summary>
+	private void RouteToPage(MainWindowViewModel recipient, RouteMessage message)
+	{
+		try
 		{
-			var page = (ViewModelBase?)sP.GetRequiredService(route.Value);
-			if (page is null) return;
+			var page = (ViewModelBase?)_serviceProvider.GetService(message.Value);
+			if (page is null)
+			{
+				_logController.Log($"No page / service found for {message.Value.ToString()}", InfoBarSeverity.Error);
+				return;
+			}
+
 			CurrentPage = page;
-		});
+		}
+		catch (Exception ex)
+		{
+			_logController.Log($"{nameof(RouteToPage)} - Exception occurred: {ex.Message}", InfoBarSeverity.Error);
+		}
 	}
 }
